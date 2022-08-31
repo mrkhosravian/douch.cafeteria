861bc0186ab680fa503227cd8fe4092f84306401
Mohammad Reza Khosravian <jomanjo80@gmail.com>
2022-08-31 15:27:00 +0430

    change json file path

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './style';
-import Menu from '../_data/menu/2022-08-31-map-menuitem-list-map-name-لاته-price-45-map-name-کاپوچینو-price-33.json';
+import menu from "../_data/menu.json"
 
 export default function App() {
 	return (
 		<div>
 			{
-				Menu.menuitem.map(item => <span>{item.name}</span>)
+				menu.pizza.map(item => <span>{item.name}</span>)
 			}
 			<h1>Hello, World!</h1>
 		</div>

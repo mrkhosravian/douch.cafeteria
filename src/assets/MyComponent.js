8b46dd19ce7d084e195d78d5cdf1a6174eda0356
Mohammad Reza Khosravian <jomanjo80@gmail.com>
2022-08-31 13:31:09 +0430

    init

---
+++ src/assets/MyComponent.js
@@ -0,0 +1,3 @@
+export default function MyComponent() {
+    return <h2>!!!</h2>
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import MyComponent from './assets/MyComponent';
+import './style';
+
+export default function App() {
+	return (
+		<div>
+			<MyComponent />
+			<h1>Hello, World!</h1>
+		</div>
+	);
+}

5ae0ae1bee80f2082cfa60e0d32dd729c34fa3b2
Mohammad Reza Khosravian <jomanjo80@gmail.com>
2022-08-31 13:41:55 +0430

    remove MyComponent

--- src/assets/MyComponent.js
+++
@@ -1,3 +0,0 @@
-export default function MyComponent() {
-    return <h2>!!!</h2>
-}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import MyComponent from './assets/MyComponent';
 import './style';
 
 export default function App() {
 	return (
 		<div>
-			<MyComponent />
 			<h1>Hello, World!</h1>
 		</div>
 	);
